51ff0eb172c4d27e1498f9e2909ee7413d90a8d8
kiichiro-tominaga <kiichiro-tominaga@kccs.co.jp>
2026-04-28 05:53:58 +0900

    feat: dynamodb single-table + ddb wrappers + local stack (fixes #12)
    - infra/dynamodb.tf: vigil テーブル (PAY_PER_REQUEST + pk/sk + gsi1
      sparse + TTL attribute=ttl + PITR + SSE + prevent_destroy)
    - infra/outputs.tf: dynamodb_table_name / _arn を export
    - web/src/lib/server/ddb.ts + scanner/src/ddb.ts:
      AWS SDK v3 DocumentClient シングルトン + getItem/putItem/queryItems/
      updateItem/deleteItem ラッパー。AWS_ENDPOINT_URL で DynamoDB Local 切替
    - web/package.json: @aws-sdk/client-dynamodb + lib-dynamodb + @types/node
      を追加、db:up/db:down/db:init スクリプトも
    - docker-compose.yml: amazon/dynamodb-local + -inMemory -sharedDb
    - scripts/init-dynamodb-local.sh: テーブル作成 + TTL spec の冪等スクリプト
    
    注: aws_dynamodb_table の hash_key/range_key は v6 で deprecated 警告。
    v7 (key_schema ブロック) で要追従。

---
+++ infra/dynamodb.tf
@@ -0,0 +1,54 @@
+resource "aws_dynamodb_table" "vigil" {
+  name         = var.project
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "pk"
+  range_key    = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  attribute {
+    name = "gsi1pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "gsi1sk"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "gsi1"
+    hash_key        = "gsi1pk"
+    range_key       = "gsi1sk"
+    projection_type = "ALL"
+  }
+
+  ttl {
+    attribute_name = "ttl"
+    enabled        = true
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Project = var.project
+  }
+}
